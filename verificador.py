82a518c80d8ceaa5a94ef1b2d37ede9c5c934669
juanpaup07 <79875217+juanpaup07@users.noreply.github.com>
2023-05-23 15:25:30 +0000

    modified:   verificador.py

--- verificador.py
+++ verificador.py
@@ -1,6 +1,6 @@
 import sys
-def compare_files(file1, file2):
 
+def compare_files(file1, file2):
     with open(file1, 'rb') as a1, open(file2, 'rb') as a2:
         c1 = a1.read()
         c2 = a2.read()
@@ -8,9 +8,11 @@ def compare_files(file1, file2):
         print("ok")
     else:
         print("nok")
-if len(sys.argv) > 1:
-    file = (sys.argv[1])
+
+if len(sys.argv) > 2:
+    file1 = sys.argv[1]
+    file2 = sys.argv[2]
+    compare_files(file1, file2)
 else:
-    print('Debe proporcionar un filepath como argumento.')
+    print('Debe proporcionar dos filepaths como argumentos.')
     sys.exit(1)
-compare_files(file,"descomprimido-elmejorprofesor.txt")
